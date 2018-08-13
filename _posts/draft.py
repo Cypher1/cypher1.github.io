4ef3a353ec4af91f75c0d2b612ee8d36cfd04980
Joshua Pratt <jp10010101010000@gmail.com>
2018-08-14 09:11:03 +1000

    Add a basic drafing script and update the home page a little

---
+++ _posts/draft.py
@@ -0,0 +1,44 @@
+#!/bin/python
+
+import sys
+import datetime
+
+DRAFT = """
+---
+published: false
+title: {title}
+category: {category}
+excerpt: | TODO:Excerpt here!
+feature_text: | TODO:Feature text here!
+feature_image: "https://picsum.photos/2560/600?image=733"
+image: "https://picsum.photos/2560/600?image=733"
+---
+
+TODO:Content goes here
+----
+"""
+
+def getArg(desc, argv):
+    if argv == []:
+        value = raw_input(desc)
+    else:
+        value = argv.pop()
+    return value
+
+def main():
+    _path = sys.argv.pop()
+    title = getArg("Title: ", sys.argv)
+    category = getArg("Categories (comma separated): ", sys.argv)
+
+    now = datetime.datetime.now()
+
+    date = now.strftime("%Y-%m-%d")
+
+    title_with_dashes = title.replace(" ", "-").lower()
+
+    filename = "{date}-{title}.md".format(date=date, title=title_with_dashes)
+
+    with open(filename, 'w') as draft:
+        draft.write(DRAFT.format(title=title, category=category))
+
+main()
--- index.md
+++ index.md
@@ -8,7 +8,7 @@ excerpt: "TODO(jopra)"
 
 In 2018 I started working full time as a software developer. This gave me some time to think about how I might try to give back to the software development community.
 
-I'm hoping to write about my experiences in the software industry, problems I've faced with tools and solutions/hacks that I've discovered (or been gifted) along the way.
+I'm hoping to write about my experiences in the software industry, problems I've faced with tools and solutions/hacks that I've discovered (or been gifted) along the way as well as a few interesting algorithms that I've learnt a bit about.
 
 I have a particular interest in 'safe' languages, machine learning, and logic engines. Specifically I'm a big fan of Haskell and Rust, but have more experience in C++ and Python.
 
