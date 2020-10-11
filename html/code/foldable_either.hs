0cae74e489880763112f47511d0decd6d00796e6
Joshua Pratt <jp10010101010000@gmail.com>
2020-10-11 20:45:42 +1100

    deployed on Sun Oct 11 20:45:40 AEDT 2020 by cypher

--- html/code/foldable_either.hs
+++
@@ -1,9 +0,0 @@
-divOrFail :: Either Int String -> Int -> Either Int String
-divOrFail (Left d) n
-  | n==0 = Right ("Cannot divide by 0")
-  | (d`mod`n)/=0 = Right ("Cannot divide "++show d++" by "++show n)
-  | otherwise = Left $ d`div`n
-divOrFail (Right e) _ = (Right e)
-
-applyAll :: Foldable f => (Either a err -> b -> Either a err) -> a -> f b -> Either a err
-applyAll func start xs = foldl func (Left start) xs
